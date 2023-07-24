47e5b6db28b236c7df281453dfc93afa75f8b13d
said benelhadj <sad.beneldje@gmail.com>
2023-07-24 14:53:44 +0200

    contactForm

--- src/main/java/com/information/learninghibernate/controllers/indexController.java
+++ src/main/java/com/information/learninghibernate/controllers/indexController.java
@@ -3,6 +3,7 @@ package com.information.learninghibernate.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class indexController {
@@ -11,5 +12,15 @@ public class indexController {
     public String index(Model model){
         model.addAttribute("title", "Learning Hibernate");
         return "page.html";
+    }
+    @GetMapping("/contact")
+    public String contact(Model model){
+        model.addAttribute("title", "Contact");
+        return "contact/form.html";
+    }
+     
+    @PostMapping("/contact")
+    public String contactSend(){
+        return "contact/form.html";
     }
 }
