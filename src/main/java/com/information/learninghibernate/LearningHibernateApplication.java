f25b7172a156f33dfb18d68eba87366402118d2a
said benelhadj <sad.beneldje@gmail.com>
2023-07-24 11:37:26 +0200

    install sprintboot

---
+++ src/main/java/com/information/learninghibernate/LearningHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.information.learninghibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/information/learninghibernate/LearningHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.information.learninghibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
